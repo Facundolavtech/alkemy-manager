e3ffa0c3d5c498c30376c6a17bb0bd94d1408520
Facundo Lavagnino <flavagnino8@gmail.com>
2022-10-03 03:00:28 -0300

    ci: add husky, commitizen, lint staged

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,3 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import * as express from 'express';
 
 const app = express();
@@ -12,7 +7,9 @@ app.get('/api', (req, res) => {
 });
 
 const port = process.env.port || 3333;
+
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
+
 server.on('error', console.error);
